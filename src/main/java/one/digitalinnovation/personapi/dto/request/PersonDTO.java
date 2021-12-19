61656e18a7fa70bb33fc370756d2409fca0d9c49
xandecoelho5 <xandecoelho5@hotmail.com>
2021-12-19 19:56:54 -0300

    Resolvido problema com mapstruct

--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import one.digitalinnovation.personapi.entity.Phone;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.Valid;
